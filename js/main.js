fa03964d908108a52dce63d5565e827b4d94816a
[thexluca] <[thexluca@gmail.com]>
2023-04-27 23:59:27 -0500

    Created JavaScript for menu button to open and close

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    
+
+    const navButton = document.getElementsByClassName("navButton")[0];
+    const navMenu = document.getElementsByClassName("navMenu")[0];
+    const navMenuOpenClose = () => navMenu.classList.toggle("navMenuOPEN");
+    navButton.onclick = navMenuOpenClose; 
+
 }
