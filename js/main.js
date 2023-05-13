867e74a382f65e8c937c1367ed1a588055b8ab54
thexluca <thexluca@gmail.com>
2023-05-12 20:13:02 -0500

    Implemented animated navIcon

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ window.onload = function() {
 
     const navButton = document.getElementsByClassName("navButton")[0];
     const navMenu = document.getElementsByClassName("navMenu")[0];
-    const navMenuOpenClose = () => navMenu.classList.toggle("navMenuOPEN");
-    navButton.onclick = navMenuOpenClose; 
+    navButton.onclick = () => navMenu.classList.toggle("navMenuOPEN");
+    
+    const navIcon = document.getElementById('navIcon');
+    navIcon.onclick = () => navIcon.classList.toggle('navIconOpen');
 
 }
