16a17a37fd76e5964c2837011e710078090fd14b
thexluca <thexluca@gmail.com>
2023-05-26 13:31:00 -0500

    Restructured nav elements. Edited CSS and JS for changes.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 window.onload = function() {
 
     const navButton = document.getElementsByClassName("navButton")[0];
+    const navIcon = document.getElementById("navIcon");
     const navMenu = document.getElementsByClassName("navMenu")[0];
-    navButton.onclick = () => navMenu.classList.toggle("navMenuOPEN");
     
-    const navIcon = document.getElementById('navIcon');
-    navIcon.onclick = () => navIcon.classList.toggle('navIconOpen');
+    navButton.onclick = () => {
+        openCloseNav();
+        navMenu.classList.toggle("show");
+    }
+    
+    // openCloseNav is a separate function to fire from multiple events
+    const openCloseNav = () => navIcon.classList.toggle("navIconOpen");
+
+    const navItem = document.getElementsByClassName("navItem");
+    for(let i=0; i<navItem.length; i++){
+        navItem[i].onclick = () => {
+            navMenu.classList.toggle("navMenuOPEN");
+            navIcon.classList.toggle("navIconOpen");
+        }
+    }
 
 }
