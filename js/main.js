f87f32c71d49f1d3ddb0e20e5c264c2aba53b916
thexluca <thexluca@gmail.com>
2023-06-02 01:11:09 -0500

    Cleaned up js script

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 window.onload = function() {
 
-    const navButton = document.getElementsByClassName("navButton")[0];
     const navIcon = document.getElementById("navIcon");
     const navMenu = document.getElementsByClassName("navMenu")[0];
-    
-    navButton.onclick = () => {
-        openCloseNav();
+    // openCloseNav is a separate function to fire from multiple events
+    const openCloseNav = () => {
+        navIcon.classList.toggle("navIconOpen");
         navMenu.classList.toggle("show");
     }
     
-    // openCloseNav is a separate function to fire from multiple events
-    const openCloseNav = () => navIcon.classList.toggle("navIconOpen");
+    const navButton = document.getElementsByClassName("navButton")[0];
+    navButton.onclick = () => {
+        openCloseNav();
+    }
 
     const navItem = document.getElementsByClassName("navItem");
     for(let i=0; i<navItem.length; i++){
         navItem[i].onclick = () => {
-            navMenu.classList.toggle("navMenuOPEN");
-            navIcon.classList.toggle("navIconOpen");
+            if(window.innerWidth < 768){
+                openCloseNav();
+            }
         }
     }
 
