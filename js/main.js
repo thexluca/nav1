d863ae53076b0f2d646f6afd7211cb166ffff471
thexluca <thexluca@gmail.com>
2023-06-02 19:49:15 -0500

    Added smooth scroll effect to pages

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 window.onload = function() {
-
+    // Variables
+    let breakpoint = 768; // css breakpoint
     const navIcon = document.getElementById("navIcon");
     const navMenu = document.getElementsByClassName("navMenu")[0];
-    // openCloseNav is a separate function to fire from multiple events
-    const openCloseNav = () => {
+    
+    // Functions
+    function openCloseNav() {
+        // openCloseNav function will fire from multiple events.
         navIcon.classList.toggle("navIconOpen");
         navMenu.classList.toggle("show");
     }
-    
-    const navButton = document.getElementsByClassName("navButton")[0];
-    navButton.onclick = () => {
-        openCloseNav();
+    function smoothScroll(event){
+        // smoothScroll function can be called on any hyperlink(a) element as it looks for the href attribute.  
+        event.preventDefault();
+        const href = this.getAttribute("href");
+        document.querySelector(href).scrollIntoView({behavior: "smooth"});
     }
 
-    const navItem = document.getElementsByClassName("navItem");
-    for(let i=0; i<navItem.length; i++){
-        navItem[i].onclick = () => {
-            if(window.innerWidth < 768){
+    // Nav Button click event.
+    const navButton = document.getElementsByClassName("navButton")[0];
+    navButton.onclick = openCloseNav;
+    
+    // Nav Pages click event.
+    const navItems = document.getElementsByClassName("navItem");
+    for(let i=0; i<navItems.length; i++){
+        navItems[i].onclick = () => {
+            if(window.innerWidth < breakpoint){
                 openCloseNav();
             }
         }
+        navItems[i].querySelector('a').onclick = smoothScroll;
     }
-
-}
+    
+}// end of window.onload
